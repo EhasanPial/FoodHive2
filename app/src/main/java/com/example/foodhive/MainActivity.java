64a77c3e61905633dd2b8ec4638a0e8429028ee5
MovieApp <pialkhanpial@gmail.com>
2021-06-19 02:19:08 +0600

    Food hive

--- app/src/main/java/com/example/foodhive/MainActivity.java
+++ app/src/main/java/com/example/foodhive/MainActivity.java
@@ -1,16 +1,70 @@
 package com.example.foodhive;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private NavigationView navigationView ;
+    private DrawerLayout drawerLayout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.main_toolbar) ;
+        navigationView = findViewById(R.id.nav_view) ;
+        drawerLayout = findViewById(R.id.main_drawwer_layout) ;
 
+        setSupportActionBar(toolbar);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+        NavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        hidetoolbar(navController);
+
+
+
+
+    }
+
+    private void hidetoolbar(NavController navController) {
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.login2 ) {
+                    toolbar.setVisibility(View.GONE);
+                    getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+                }
+                else {
+                    toolbar.setVisibility(View.VISIBLE);
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+                }
+
+            }
+        });
     }
 }
