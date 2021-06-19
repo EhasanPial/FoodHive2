95746a41790d1991a1e57a62019d36c947ca2390
MovieApp <pialkhanpial@gmail.com>
2021-06-19 23:42:16 +0600

    Food hive

--- app/src/main/java/UI/SignUp.java
+++ app/src/main/java/UI/SignUp.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +15,27 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.foodhive.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Model.UsersModel;
 
 
 public class SignUp extends Fragment {
     // --- UI --//
     private EditText username, email, pass, address, phone;
     private Button signUp;
+
+    // --- Vairable --- //
+    private FirebaseAuth firebaseAuth ;
+    private DatabaseReference databaseReference ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +55,13 @@ public class SignUp extends Fragment {
         address = view.findViewById(R.id.addressSignUp);
         phone = view.findViewById(R.id.phoneSignUp);
         signUp = view.findViewById(R.id.buttonSignUp);
+
+
+        // ---------- Firebase --------- //
+        firebaseAuth = FirebaseAuth.getInstance() ;
+        databaseReference = FirebaseDatabase.getInstance().getReference("Info") ;
+
+        NavController navController = Navigation.findNavController(view) ;
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +92,23 @@ public class SignUp extends Fragment {
                     phone.requestFocus();
                 }
 
+                firebaseAuth.signInWithEmailAndPassword(emailtext,passtext).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        UsersModel usersModel = new UsersModel(nameText,emailtext,passtext,addresstext,phonetext) ;
+                        databaseReference.child("Users Info").child(phonetext).setValue(usersModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                navController.navigate(R.id.action_signUp_to_homeFragment);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(view,e.getMessage().toString(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
 
             }
         });
